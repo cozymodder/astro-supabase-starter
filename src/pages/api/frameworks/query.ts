16ec0682e53834c4dfdc133d2785f24bde2c8297
danchan1119 <danchan1119@gmail.com>
2025-09-27 21:21:23 +0800

    Create query.ts

---
+++ src/pages/api/frameworks/query.ts
@@ -0,0 +1,27 @@
+import type { APIRoute } from "astro";
+import { supabase } from "../../../../utils/database";
+
+export const prerender = false;
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+  const mobile = formData.get("mobile")?.toString();
+  if (!mobile) {
+    return new Response("Mobile number is required", { status: 400 });
+  }
+
+  if (supabase) {
+    const { data: framework } = await supabase
+      .from("frameworks")
+      .select("*")
+      .eq("mobile", mobile)
+      .single();
+
+    if (!framework) {
+      return new Response(`Where'd that pet go?`, { status: 404 });
+    }
+    return redirect(`/frameworks/${framework.id}`, 303);
+  }
+
+  return new Response("No supabase url provided", { status: 400 });
+};
