c28abe3e8e38158eca65237bd0732c172807b6f7
danchan1119 <danchan1119@gmail.com>
2025-09-27 21:22:12 +0800

    Update query.ts

--- src/pages/api/frameworks/query.ts
+++ src/pages/api/frameworks/query.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro";
-import { supabase } from "../../../../utils/database";
+import { supabase } from "../../../utils/database";
 
 export const prerender = false;
 
